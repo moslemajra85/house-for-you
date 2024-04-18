56d06f34334c747a050fa5740b806859b58c9afe
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-04-18 17:28:03 +0100

    Sign in page

---
+++ src/assets/svg/ArrowRightIcon.jsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+
+const ArrowRightIcon = forwardRef((props, ref) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="34px"
+      viewBox="0 0 24 24"
+      width="34px"
+      fill="#fff"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+    </svg>
+  );
+});
+
+export default ArrowRightIcon;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import ArrowRightIcon from '../assets/svg/ArrowRightIcon';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-export default SignIn
+  const { email, password } = formData;
+
+  const onChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back</p>
+        </header>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log(formData);
+          }}
+        >
+          <input
+            type="email"
+            id="email"
+            className="emailInput"
+            placeholder="Email"
+            value={formData.email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              className="passwordInput"
+              placeholder="Password"
+              value={formData.password}
+              onChange={onChange}
+            />
+            <img
+              onClick={() => setShowPassword(!showPassword)}
+              className="showPassword"
+              src={visibilityIcon}
+            />
+          </div>
+          <Link to="/forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+          <div className="signInBar">
+            <p className="signInText">Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon />
+            </button>
+          </div>
+        </form>
+        {/* Google oAuth */}
+        <Link to="/sign-up" className="registerLink">
+          Sign Up Instead
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
