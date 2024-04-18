69a0e9ab3505602d98c97fff473eec27bd0d1a77
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-04-18 09:44:20 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import ForgotPassword  from "./pages/ForgotPassword"
 
 const App = () => {
-  return <div>App</div>;
+  return <>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Explore />} />
+      <Route path='/offers' element={<Offers />} />
+      <Route path='/profile' element={<SignIn />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+    {/* navbar */}
+    </Routes>
+  </Router>
+
+  </>;
 };
 
 export default App;
